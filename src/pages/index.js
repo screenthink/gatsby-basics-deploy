d77bebedce322df39d4b38d873e8544b10dbba1e
Marlon Lulgjuraj <marlon@screenthink.com>
2019-09-03 12:30:37 -0400

    Edited homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,15 @@ import SEO from "../components/seo"
 export default () => (
   <Layout>
     <SEO title="Welcome" />
-    <h2>Welcome</h2>
-    <p>Etiam porta sem malesuada magna mollis euismod.</p>
+    <h2>Welcome!</h2>
+    <p>
+      Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Fusce
+      dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut
+      fermentum massa justo sit amet risus. Nulla vitae elit libero, a pharetra
+      augue. Donec sed odio dui. Donec id elit non mi porta gravida at eget
+      metus. Cum sociis natoque penatibus et magnis dis parturient montes,
+      nascetur ridiculus mus. Morbi leo risus, porta ac consectetur ac,
+      vestibulum at eros.
+    </p>
   </Layout>
 )
